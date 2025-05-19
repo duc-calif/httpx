c86a23928e041e61e33b0c7e68464db0d4121391
Duc Nguyen <ducnh@calif.io>
2025-05-19 14:09:56 +0700

    add tests

--- cmd/integration-test/generate.sh
+++ cmd/integration-test/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-idtoken=$(curl --fail-with-body -sSL -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=octo-sts.dev)
+idtoken=$(curl -sSL -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=octo-sts.dev)
 echo $idtoken|gzip -v9|base64 -w0
